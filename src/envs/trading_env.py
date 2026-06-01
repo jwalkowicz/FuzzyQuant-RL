1db19fc4984d36b5c932593f8ef915370c467496
walczykk <walkowiczjj@gmail.com>
2026-06-02 00:02:16 +0200

    Implemented TradingEnv

--- src/envs/trading_env.py
+++
@@ -1,9 +0,0 @@
-import gymnasium as gym
-
-
-class TradingEnv(gym.Env):
-    def __init__(self, data, initial_balance=10000):
-        self.data = data
-        self.initial_balance = initial_balance
-        self.action_space = gym.spaces.Discrete(4)
-        self.observation_space = gym.spaces.Discrete(9)
